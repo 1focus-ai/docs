408a72f229d8fba53e4887680e78c7b274af3b16
Nikita <github@nikiv.dev>
2025-10-07 13:55:50 +0300

    .

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -8,6 +8,7 @@ import type { BaseLayoutProps } from "fumadocs-ui/layouts/shared"
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
+  githubUrl: "https://github.com/1focus-ai/docs",
   nav: {
     title: "docs.1focus.ai",
   },
